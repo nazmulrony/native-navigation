63b5930278109ed8ffa2ab5e298fe8650d131dbc
Shahadat_Robin <robin@corexlab.dev>
2023-01-10 10:34:27 +0600

    Drawer Navigator added

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealOverviewScreen from "./screens/MealOverviewScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Categories" component={CategoriesScreen} />
+            <Drawer.Screen name="Categories" component={CategoriesScreen} />
+        </Drawer.Navigator>
+    );
+};
 
 export default function App() {
     return (
@@ -21,11 +32,13 @@ export default function App() {
                     }}
                 >
                     <Stack.Screen
-                        name="MealsCategories"
-                        component={CategoriesScreen}
-                        options={{
-                            title: "All Categories",
-                        }}
+                        name="Drawer"
+                        component={DrawerNavigator}
+                        options={
+                            {
+                                // title: "All Categories",
+                            }
+                        }
                     />
                     <Stack.Screen
                         name="MealOverview"
