f028afc03fa01fd1968f61197098c1926dfd082b
Shahadat_Robin <robin@corexlab.dev>
2023-01-12 10:44:01 +0600

    favorites meals displayed using context api

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,11 +1,37 @@
-import { Text, View } from "react-native";
+import { useContext } from "react";
+import MealsList from "../components/MealsList/MealsList";
+import { FavoriteContext } from "../store/conetext/favorite-context";
+import { MEALS } from "../data/dummy-data";
+import { StyleSheet, Text, View } from "react-native";
+import { color } from "react-native-reanimated";
 
 const FavoriteScreen = () => {
-    return (
-        <View>
-            <Text>The Favorite screen</Text>
-        </View>
-    );
+    const { ids } = useContext(FavoriteContext);
+    const favoriteMeals = MEALS.filter((meal) => ids.includes(meal.id));
+    // console.log(favoriteMeals);
+
+    if (!favoriteMeals.length) {
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>You have no favorite meals yet!</Text>
+            </View>
+        );
+    }
+
+    return <MealsList items={favoriteMeals} />;
 };
 
 export default FavoriteScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
+    },
+});
